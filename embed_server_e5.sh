376e2cd029eafd27641dac082e8ace53dc8e97df
Pedro Martins <pedro.martins@dtx-colab.pt>
2025-07-07 13:19:31 +0100

    Update embed_server_e5.sh

--- embed_server_e5.sh
+++ embed_server_e5.sh
@@ -1,5 +1,5 @@
 
-until vllm serve intfloat/multilingual-e5-large-instruct --port 8082 --gpu-memory-utilization 0.1; do
+until vllm serve intfloat/multilingual-e5-large-instruct --port 8081 --gpu-memory-utilization 0.1; do
   echo "vLLM serve failed to start, retrying in 5 seconds..."
   sleep 5
 done
