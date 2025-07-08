d1039b5f062292853d3a08af8de1bb4c28a1d93f
Pedro Martins <pedro.martins@dtx-colab.pt>
2025-07-08 14:11:36 +0100

    Update llm_server.sh

--- llm_server.sh
+++ llm_server.sh
@@ -1,6 +1,6 @@
 until vllm serve neuralmagic-ent/Llama-3.3-70B-Instruct-quantized.w8a8 \
   --api-key token-abc123 \
-  --port 8080 \
+  --port 8081 \
   --enable-auto-tool-choice \
   --tool-call-parser llama3_json \
   --chat-template tool_chat_template_llama3.1_json.jinja \
