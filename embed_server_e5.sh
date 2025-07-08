d6018ec89110ac8507a7107730e2dc5bc5126d1b
Pedro Martins <pedro.martins@dtx-colab.pt>
2025-07-08 09:33:24 +0100

    Delete embed_server_e5.sh

--- embed_server_e5.sh
+++
@@ -1,5 +0,0 @@
-
-until vllm serve intfloat/multilingual-e5-large-instruct --port 8081 --gpu-memory-utilization 0.1; do
-  echo "vLLM serve failed to start, retrying in 5 seconds..."
-  sleep 5
-done
