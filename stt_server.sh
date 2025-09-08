519ead75a45d174880c146a8779166fdf5f774e6
Pedro Martins <pedro.martins@dtx-colab.pt>
2025-09-08 08:07:20 +0100

    Update stt_server.sh

--- stt_server.sh
+++ stt_server.sh
@@ -1,7 +1,6 @@
 until vllm serve fsicoli/whisper-large-v3-pt-cv19-fleurs \
  --api-key token-abc123 \
  --port 8082 \
- --disable-log-requests \
  --gpu-memory-utilization 0.2 \
  --tokenizer-mode slow; do
   echo "vLLM serve failed to start, retrying in 5 seconds..."
